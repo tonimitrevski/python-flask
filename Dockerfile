ad832f454f66cf3abf871f0b07a4b95303876002
Toni Mitrevski <toni@stativa.com.mk>
2018-07-24 21:54:38 +0200

    add postgre database

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 
+RUN pip install flask-sqlalchemy
 # If STATIC_INDEX is 1, serve / with /static/index.html directly (or the static URL configured)
 #ENV STATIC_INDEX 1
 ENV STATIC_INDEX 0
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@localhost:5432/flask'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
